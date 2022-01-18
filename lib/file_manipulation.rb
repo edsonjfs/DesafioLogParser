0ffde3dbfa864ccc3ed7111d9cf423450d42f4d8
edsonjfs <edsonjosefs@gmail.com>
2022-01-18 17:14:47 -0300

    fix: moved a block of code into initialize, and changed tests

--- lib/file_manipulation.rb
+++ lib/file_manipulation.rb
@@ -2,22 +2,21 @@ class FileManipulation
   
   def initialize(path_to_file)
     @path_to_file = path_to_file
-    File.open(path_to_file)
+    @file = File.open(path_to_file)
+    
+    begin
+      file = open(@path_to_file)
+      file.close
+    rescue => exception
+      puts "#{$!.class}: #{$!.message}"
+    end
   end
 
-  def read_especific_line(line_number, path_to_file)
-    IO.readlines(path_to_file)[line_number]
+  def read_especific_line(line_number)
+    IO.readlines(@path_to_file)[line_number]
   end
 
-  def is_closed(path_to_file)
-    File.open(path_to_file).closed?
+  def exists
+    File.exists?(@path_to_file)
   end
-
-  begin
-    file = open("games.log")
-    file.close
-  rescue => exception
-    puts "#{$!.class}: #{$!.message}"
-  end
-
 end
--- spec/file_manipulation_spec.rb
+++ spec/file_manipulation_spec.rb
@@ -3,23 +3,22 @@ require_relative "../lib/file_manipulation"
   describe FileManipulation do
     describe "#print_especific_line" do
       it "print specific line from a file" do
-        printed_line = FileManipulation.new("games.log").read_especific_line(1,"games.log")
+        printed_line = FileManipulation.new("games.log").read_especific_line(1)
         expect(printed_line).to eql("  0:00 InitGame: \\sv_floodProtect\\1\\sv_maxPing\\0\\sv_minPing\\0\\sv_maxRate\\10000\\sv_minRate\\0\\sv_hostname\\Code Miner Server\\g_gametype\\0\\sv_privateClients\\2\\sv_maxclients\\16\\sv_allowDownload\\0\\dmflags\\0\\fraglimit\\20\\timelimit\\15\\g_maxGameClients\\0\\capturelimit\\8\\version\\ioq3 1.36 linux-x86_64 Apr 12 2009\\protocol\\68\\mapname\\q3dm17\\gamename\\baseq3\\g_needpass\\0\n")
       end
     end
     
     describe "#print_especific_line" do
         it "print specific line from a false log file" do
-          printed_line = FileManipulation.new("false_log.log").read_especific_line(1,"false_log.log")
+          printed_line = FileManipulation.new("false_log.log").read_especific_line(1)
           expect(printed_line).to eql(nil)
         end
       end
 
   describe "#open_file" do 
     it "test if the file can be opened and exists" do
-      file = FileManipulation.new("games.log").is_closed("games.log")
-      expect(file).to eql(false)
+      file = FileManipulation.new("games.log").exists
+      expect(file).to eql(true)
     end
   end
-
 end
