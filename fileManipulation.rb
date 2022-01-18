257335a64057834ce37946eab97d87144b8c1ce3
edsonjfs <edsonjosefs@gmail.com>
2022-01-18 11:00:06 -0300

    refactored Filemanipulation class to change print_especific_line method to actualy print a line and not just read

--- fileManipulation.rb
+++ fileManipulation.rb
@@ -1,15 +1,16 @@
 class FileManipulation
 
     def print_especific_line(line_number, path_to_file)
-        IO.readlines(path_to_file)[line_number]
+        puts IO.readlines(path_to_file)[line_number]
     end
-
+    
     def open_file(path_to_file)
         File.open(path_to_file)
     end
 
     begin
         file = open("games.log")
+        file.close
     rescue => exception
         puts "#{$!.class}: #{$!.message}"
     end
