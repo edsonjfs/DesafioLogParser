366c3e1bdde4feee7f7b5afa8462c1fe492adde0
edsonjfs <edsonjosefs@gmail.com>
2022-01-21 10:41:23 -0300

    feat: added method file_lines_to_json and the tests for this method

--- lib/file_manipulation.rb
+++ lib/file_manipulation.rb
@@ -1,12 +1,19 @@
-class FileManipulation
-  
+require 'json'
+
+class FileManipulation 
   def initialize(path_to_file)
     @path_to_file = path_to_file
+    @number_of_lines = File.readlines(@path_to_file).size
     file = File.open(@path_to_file)
     file.close
   end
-  
+
   def first_line
     File.readlines(@path_to_file)[0].chomp
+  end
+
+  def file_lines_to_json
+    hash = { @path_to_file => { 'lines' => @number_of_lines } }
+    hash.to_json
   end
 end
--- spec/file_manipulation_spec.rb
+++ spec/file_manipulation_spec.rb
@@ -1,16 +1,24 @@
-require_relative "../lib/file_manipulation"
+require_relative '../lib/file_manipulation'
 
 describe FileManipulation do
-  describe "#first_line" do
-    it "print fisrt line from a file" do
-      printed_line = FileManipulation.new("false_games.log").first_line
-      expect(printed_line).to eql("  0:00 ------------------------------------------------------------")
+  describe '#first_line' do
+    it 'print fisrt line from a file' do
+      printed_line = FileManipulation.new('false_games.log').first_line
+      expect(printed_line).to eql('  0:00 ------------------------------------------------------------')
     end
   end
 
-  describe "#initialize" do 
-    it "tests if the file does exists" do
-    expect{FileManipulation.new("invalid_path_file")}.to raise_error(Errno::ENOENT)
+  describe '#initialize' do
+    it 'tests if the file does exists' do
+      expect { FileManipulation.new('invalid_path_file') }.to raise_error(Errno::ENOENT)
+    end
+  end
+
+  describe 'file_lines_to_json' do
+    it 'tests if the method is returning correctly the JSON object' do
+      file = FileManipulation.new('false_games.log')
+      json = { 'false_games.log' => { 'lines' => 1 } }.to_json
+      expect(file.file_lines_to_json).to eql(json)
     end
   end
 end
